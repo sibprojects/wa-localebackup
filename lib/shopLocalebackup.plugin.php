688b5954d3984775cd283480115f46c75fb80c85
siberian <siberian@sibprojects.com>
2023-04-13 00:59:57 +0100

    add main code plugin

--- lib/shopLocalebackup.plugin.php
+++ lib/shopLocalebackup.plugin.php
@@ -2,14 +2,14 @@
 
 class shopLocalebackupPlugin extends shopPlugin
 {
-	public static function getApps()
-	{
-		return ['blog','mailer','mylang','shop','site'];
-	}
+    public static function getApps()
+    {
+        return ['blog', 'mailer', 'mylang', 'shop', 'site'];
+    }
 
-	public static function getLocales()
-	{
-		return ['en_US','ru_RU','uk_UA'];
-	}
+    public static function getLocales()
+    {
+        return ['en_US', 'ru_RU', 'uk_UA'];
+    }
 
 }
