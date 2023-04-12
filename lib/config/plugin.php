512037974350671d0947bb06e5ed27bcd8f66099
siberian <siberian@sibprojects.com>
2023-04-13 00:54:50 +0100

    update config

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,12 +1,12 @@
 <?php
 
-return array(
-    'name' => 'Сохранение и восстановление локалей',
-    'description' => 'Автор: SibProjects.com',
-    'vendor'=>'0',
-    'version'=>'1.0.0',
+return [
+    'name'          => 'Сохранение и восстановление локалей',
+    'description'   => 'Автор: SibProjects.com',
+    'vendor'        => '0',
+    'version'       => '1.0.0',
     'shop_settings' => true,
-    'handlers' => array(
-    ),
-);
+    'handlers'      => [
+    ],
+];
 //EOF
