2b472c26100df2a26dd1149c1b690608dc675f35
siberian <siberian@sibprojects.com>
2023-06-07 17:17:55 +0100

    Update

--- lib/actions/shopLocalebackupPluginBackendSave.controller.php
+++ lib/actions/shopLocalebackupPluginBackendSave.controller.php
@@ -6,25 +6,25 @@ class shopLocalebackupPluginBackendSaveController extends waController
     {
         $path = wa()->getDataPath('localebackup/', false, 'shop');
 
-        $apps = shopLocalebackupPlugin::getApps();
+        $apps    = shopLocalebackupPlugin::getApps();
         $locales = shopLocalebackupPlugin::getLocales();
 
         foreach ($apps as $app) {
-            if (!file_exists($path . $app)) {
-                mkdir($path . $app);
-                mkdir($path . $app . '/locale');
+            if (!file_exists($path.$app)) {
+                mkdir($path.$app);
+                mkdir($path.$app.'/locale');
                 foreach ($locales as $locale) {
-                    mkdir($path . $app . '/locale/' . $locale);
-                    mkdir($path . $app . '/locale/' . $locale . '/LC_MESSAGES');
+                    mkdir($path.$app.'/locale/'.$locale);
+                    mkdir($path.$app.'/locale/'.$locale.'/LC_MESSAGES');
                 }
             }
 
             $appPath = wa()->getAppPath(null, $app);
             foreach ($locales as $locale) {
-                @unlink($path . $app . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.po');
-                @unlink($path . $app . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.mo');
-                @copy($appPath . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.po', $path . $app . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.po');
-                @copy($appPath . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.mo', $path . $app . '/locale/' . $locale . '/LC_MESSAGES/' . $app . '.mo');
+                @unlink($path.$app.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.po');
+                @unlink($path.$app.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.mo');
+                @copy($appPath.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.po', $path.$app.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.po');
+                @copy($appPath.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.mo', $path.$app.'/locale/'.$locale.'/LC_MESSAGES/'.$app.'.mo');
             }
         }
 
